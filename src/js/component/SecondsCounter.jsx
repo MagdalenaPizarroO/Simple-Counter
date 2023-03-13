4077697ca401834a5b101916dfc14bfeb64c5c60
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-03-13 22:58:18 +0000

    componentes linkeados, no quiere ordenar para el lado :(

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function SecondsCounter() {
+  return (
+    <div className="container-fluid ">
+      <div className="Row ">
+        <div className="col-1 ">
+          <i class="fas fa-clock"></i>
+        </div>
+        <div className="col-1">a</div>
+        <div className="col-1">b</div>
+        <div className="col-1">c</div>
+        <div className="col-1">d</div>
+        <div className="col-1">e</div>
+        <div className="col-1">f</div>
+      </div>
+    </div>
+  );
+}
+
+export default SecondsCounter;
